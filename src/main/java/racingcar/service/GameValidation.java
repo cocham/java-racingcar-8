f387f4cd71769205d93dc70fb61666ec01eccd00
cocham <mh020805@naver.com>
2025-10-22 16:32:01 +0900

    fix: MAXIMUM_RACE_TIME 지정

--- src/main/java/racingcar/service/GameValidation.java
+++ src/main/java/racingcar/service/GameValidation.java
@@ -4,6 +4,7 @@ import racingcar.exception.InvalidRaceException;
 
 public class GameValidation {
     final int MINIMUM_RACE_TIME = 1;
+    final int MAXIMUM_RACE_TIME = 50;
 
     public void validateRaceTime(String time){
         int num = parseRaceTime(time);
@@ -11,8 +12,8 @@ public class GameValidation {
     }
 
     public void validateRace(int num){
-        if (num < MINIMUM_RACE_TIME) {
-            throw new InvalidRaceException(MINIMUM_RACE_TIME);
+        if (num < MINIMUM_RACE_TIME || num > MAXIMUM_RACE_TIME) {
+            throw new InvalidRaceException(MINIMUM_RACE_TIME, MAXIMUM_RACE_TIME);
         }
     }
 

3287ef6b7041ef4318b3fc9ac6fd44112c159522
cocham <mh020805@naver.com>
2025-10-23 17:33:14 +0900

    feat: 자동차 이름 및 시도 횟수 유효성 검사 기능 구현

--- src/main/java/racingcar/service/GameValidation.java
+++ src/main/java/racingcar/service/GameValidation.java
@@ -3,21 +3,22 @@ package racingcar.service;
 import racingcar.exception.InvalidRaceException;
 
 public class GameValidation {
-    final int MINIMUM_RACE_TIME = 1;
-    final int MAXIMUM_RACE_TIME = 50;
+    private static final int MINIMUM_RACE_TIME = 1;
+    private static final int MAXIMUM_RACE_TIME = 50;
 
-    public void validateRaceTime(String time){
+    public int returnRaceTime(String time){
         int num = parseRaceTime(time);
-        validateRace(num);
+        validateRaceTime(num);
+        return num;
     }
 
-    public void validateRace(int num){
+    private void validateRaceTime(int num){
         if (num < MINIMUM_RACE_TIME || num > MAXIMUM_RACE_TIME) {
             throw new InvalidRaceException(MINIMUM_RACE_TIME, MAXIMUM_RACE_TIME);
         }
     }
 
-    public int parseRaceTime(String time) {
+    private int parseRaceTime(String time) {
         try {
             return Integer.parseInt(time);
         } catch (NumberFormatException e) {

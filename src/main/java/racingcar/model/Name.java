7efe26c6bda36789f79d332c34094f510db463d3
cocham <mh020805@naver.com>
2025-10-27 16:14:10 +0900

    fixup! refactor: Car 이름 검증 로직을 Name 레코드로 캡슐화

---
+++ src/main/java/racingcar/model/Name.java
@@ -0,0 +1,36 @@
+package racingcar.model;
+
+import racingcar.exception.InvalidLengthException;
+import racingcar.exception.InvalidNameException;
+
+import java.util.regex.Pattern;
+
+public record Name(String name) {
+    private static final String VALID_NAME_REGEX = "^[a-zA-Z0-9가-힣]+$";
+    private static final Pattern VALID_NAME_PATTERN = Pattern.it compile(VALID_NAME_REGEX);
+    private static final int VALID_NAME_MAX_LENGTH = 5;
+
+    public Name {
+        name = name.trim();
+        validateName(name);
+    }
+
+    private void validateName(String name) {
+        int nameLength = name.length();
+        validateLength(nameLength);
+        validateNameForm(name);
+    }
+
+    private void validateLength(int length) {
+        if (length > VALID_NAME_MAX_LENGTH) {
+            throw new InvalidLengthException(name, length);
+        }
+    }
+
+    private void validateNameForm(String name) {
+        if (!VALID_NAME_PATTERN.matcher(name).matches()) {
+            throw new InvalidNameException(name);
+        }
+    }
+
+}
